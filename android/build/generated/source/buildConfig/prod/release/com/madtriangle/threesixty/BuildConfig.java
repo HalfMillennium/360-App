75e94b4e845edddd96d5c1cd57294f1ce4c8a297
Millennium <glc2266@gmail.com>
2019-05-15 19:53:11 -0400

    v0.0

---
+++ android/build/generated/source/buildConfig/prod/release/com/madtriangle/threesixty/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.madtriangle.threesixty;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String APPLICATION_ID = "com.digitalnode.threesixty";
+  public static final String BUILD_TYPE = "release";
+  public static final String FLAVOR = "prod";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
